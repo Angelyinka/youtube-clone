1dc37d0efdfc67047c682afd9731b7090f43737f
Angel <angelyinkabest@gmail.com>
2025-01-07 22:47:03 +0100

    commit

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const API_KEY = 'AIzaSyDaBoZ6B6yAdmG0DqcUcwd4X5IzVePKSF4';
+
+export const valueConverter = (value) => {
+  if (value>=1000000)
+  {
+    return Math.floor(value/1000000)+"M";
+  }
+  else if(value>=1000)
+  {
+    return Math.floor(value/1000)+"k"; 
+  }
+  else{
+    return value;
+  }
+};
+
+

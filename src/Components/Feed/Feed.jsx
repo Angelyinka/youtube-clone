7059f17a0e40bd80c3f188a46dc52abca60afde0
Philip <pokhae@plateaumed.com>
2025-01-10 14:56:18 +0100

    [feat]: setup the use of .env for VITE

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,45 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import moment from "moment";
-import './Feed.css';
-import { Link } from 'react-router-dom';
-import { valueConverter } from '../../data';
-import addKeyToUrl from '../../utils/add_key_to_url';
-
-
+import "./Feed.css";
+import { Link } from "react-router-dom";
+import { valueConverter } from "../../data";
+import addKeyToUrl from "../../utils/add_key_to_url";
 
 const Feed = ({ category }) => {
+  const [data, setData] = useState([]);
 
-  const [data,setData] =useState([]);
+  const fetchData = async () => {
+    const videoList_url = addKeyToUrl(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}`
+    );
+    await fetch(videoList_url)
+      .then((response) => response.json())
+      .then((data) => setData(data.items));
+  };
 
- const fetchData = async () =>{
-   const videoList_url = addKeyToUrl(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}`);
-   await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
- }
+  useEffect(() => {
+    fetchData();
+  }, [category]);
 
- useEffect(()=>{
-     fetchData();
- },[category])
-
-
-
- return (
-   <div className="feed">
-     {data.map((item,index)=>{
-       return (
-         <Link to={`video/${item.snippet.categoryID}/${item.id}`} className='card'>
-   <img src={item.snippet.thumbnails.medium.url} alt="" />
-   <h2>{item.snippet.title}</h2>
-   <h3>{item.snippet.channelTitle}</h3>
-   <p>{valueConverter(item.statistics.viewCount)} views &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
-   </Link>
-       )
-     })}
-
- </div>
+  return (
+    <div className="feed">
+      {data.map((item, index) => {
+        return (
+          <Link
+            key={index}
+            to={`video/${item.snippet.categoryID}/${item.id}`}
+            className="card"
+          >
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <h2>{item.snippet.title}</h2>
+            <h3>{item.snippet.channelTitle}</h3>
+            <p>
+              {valueConverter(item.statistics.viewCount)} views &bull;{" "}
+              {moment(item.snippet.publishedAt).fromNow()}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
   );
 };
-
-
-
 
 export default Feed;
--- src/utils/add_key_to_url.js
+++ src/utils/add_key_to_url.js
@@ -1,6 +1,5 @@
-const addKeyToUrl = (url)=>{
-
-  const apiKey = process.env.REACT_APP_API_KEY;
+const addKeyToUrl = (url) => {
+  const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 
   return `${url}&key=${apiKey}`;
 };
